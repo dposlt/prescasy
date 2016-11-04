0126b3942eb2fd1d5a9b408abeb131f4296d8760
dposlt <david.poslt@gmail.com>
2016-11-04 14:03:59 +0100

    fix month

--- date_month.py
+++ date_month.py
@@ -13,4 +13,4 @@ class date:
         index = date.GetMonth()
         months = ['leden','únor','březen','duben','květen','červen','červenec','srpen','září','říjen','listopad','prosinec']
         
-        return months[index + 1].upper()
+        return months[index - 1].upper()
