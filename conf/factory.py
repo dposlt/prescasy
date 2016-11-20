85100d66d150a4dcc971b8b1ab8f09f10f0e480a
moula <david.poslt@gmail.com>
2016-11-20 15:29:39 +0100

    fix factory

--- conf/factory.py
+++ conf/factory.py
@@ -1,21 +1,22 @@
 import unittest
+import config
 
 class factory(unittest.TestCase):
 
     def test_host(self):
-        import config
+
         self.assertEquals(config.parser.host(),'localhost')
 
     def test_db(self):
-        import config
+
         self.assertEquals(config.parser.db(),'esprescasy')
 
     def test_user(self):
-        import config
+
         self.assertEquals(config.parser.user(),'root')
 
     def test_pass(self):
-        import config
+
         self.assertEquals(config.parser.userPass(),'postgres')
 
 if __name__ == '__main__':
